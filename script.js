42ccef8f3612f2341d5ec3b0e01b05b3fbd454e0
Pushpa Kumari <68471073+pushpakumari5117@users.noreply.github.com>
2021-02-05 21:34:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const sounds=['applause', 'boo', 'cheering', 'clapping', 'water', 'thunder']
+sounds.forEach(sound => {
+  const btn=document.createElement('button')
+  btn.classList.add('btn')
+  btn.innerText=sound
+  btn.addEventListener('click', ()=>{
+    stopSongs()
+    document.getElementById(sound).play()
+  })
+  document.getElementById('buttons').
+  appendChild(btn)
+})
+function stopSongs() {
+  sounds.forEach(sound => {
+    const song=document.getElementById(sound)
+    song.pause()
+    song.currentTime=0;
+  })
+}
